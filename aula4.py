224714f80ea41209266f7ce3ec53e703c282daff
Ezequiel Alteredo <ezequielcaldas19@gmail.com>
2023-04-19 19:55:07 -0300

    atualizado aula 4

--- aula4.py
+++ aula4.py
@@ -2,3 +2,11 @@ idade = int(input("digite a sua idade, e lhe mostrarei em #, hahahaha: "))
 
 print("sua idade em # é ")
 print("#"*idade)
+
+#manipulando textos(string)
+
+
+print (f"o total de letras é {len (texto)}") # len() vem  de length, que significa total
+
+print(texto,upper())# upper ()coloca todo texto em maiúscilo
+print(texto.capitalize())# coloca a 1ª letra em maiúsculo
